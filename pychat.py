b1d18a6a41103e060e42a2d877663ac0e4cf0b9a
wuhk76 <wuhk76@gmail.com>
2025-06-10 13:56:40 -0700

    Add files via upload

--- pychat.py
+++ pychat.py
@@ -2,19 +2,28 @@ import os
 import time
 import subprocess
 from scapy.all import *
+from cryptography.fernet import Fernet as fernet
+
 class Pychat:
+
     def __init__(self):
         self.dest = ''
         self.iface = ''
+        self.key = ''
+
     def send(self, message):
+        if self.key != '':
+            cipher = fernet(self.key)
+            message = cipher.encrypt(message.encode('utf-8', errors = 'ignore'))
         term = subprocess.run(f'ip link show {self.iface}', capture_output = True, text = True, shell = True)
         term = term.stdout.split('radiotap')
         term = term[1]
         term = term.split('brd')
         term = term[0]
         mac = term.strip()
-        packet = RadioTap() / Dot11(type = 2, subtype = 0, addr1 = self.dest, addr2 = mac, addr3 = mac) / LLC() / SNAP() / Raw(load =message)
+        packet = RadioTap() / Dot11(type = 2, subtype = 0, addr1 = self.dest, addr2 = mac, addr3 = mac) / LLC() / SNAP() / Raw(load = message)
         sendp(packet, iface = self.iface, verbose = False)
+
     def receive(self):
         term = subprocess.run(f'ip link show {self.iface}', capture_output = True, text = True, shell = True)
         term = term.stdout.split('radiotap')
@@ -28,16 +37,25 @@ class Pychat:
             packet = packet[0]
             if packet.haslayer(Raw) and packet.haslayer(Dot11) and packet.addr1 == mac:
                 rmessage = packet[Raw]
-                rmessage = rmessage.load.decode('utf-8', errors = 'ignore')
+        if self.key != '':
+            cipher = fernet(self.key)
+            rmessage = cipher.decrypt(rmessage.load).decode()
+        else:
+            rmessage = rmessage.load.decode('utf-8', errors = 'ignore')
         return rmessage
-    def dialup(self, dest, iface):
+    
+    def dialup(self, dest, iface, key):
         self.dest = dest
         self.iface = iface
+        self.key = key.encode()
         print(f'Connected to {self.dest} on interface {self.iface}')
+
 pychat = Pychat()
-pychat.dialup(input('Enter destination MAC address: '), input('Enter interface name: '))
+pychat.dialup(input('Enter destination MAC address: '), input('Enter interface name: '), input('Enter encryption key (leave blank for no encryption): '))
+print('')
 command = ''
 running = True
+
 while running:
     message = input('(you):')
     if message == ':q':
@@ -47,8 +65,11 @@ while running:
     elif message == ':r':
         pychat.send(' ')
         print(f'({pychat.dest}):{pychat.receive()}')
-    elif message == ':s':
-        pychat.send(input('(you):'))
+    elif message[0:2] == ':s':
+        info = message.split(' ')
+        del info[0]
+        info = ' '.join(info)
+        pychat.send(info)
     elif message == ':rr':
         while running:
             print(f'({pychat.dest}):{pychat.receive()}')
