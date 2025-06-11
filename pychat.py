742dae4dddac1aba43089aa74a55ebe9507db164
wuhk76 <wuhk76@gmail.com>
2025-06-11 15:22:55 -0700

    Add files via upload

--- pychat.py
+++ pychat.py
@@ -12,9 +12,11 @@ class Pychat:
         self.key = ''
 
     def send(self, message):
+
         if self.key != '':
             cipher = fernet(self.key)
             message = cipher.encrypt(message.encode('utf-8', errors = 'ignore'))
+
         term = subprocess.run(f'ip link show {self.iface}', capture_output = True, text = True, shell = True)
         term = term.stdout.split('radiotap')
         term = term[1]
@@ -32,16 +34,21 @@ class Pychat:
         term = term[0]
         mac = term.strip()
         rmessage = ''
+
         while len(rmessage) < 1:
             packet = sniff(iface = self.iface, count = 1)
             packet = packet[0]
+
             if packet.haslayer(Raw) and packet.haslayer(Dot11) and packet.addr1 == mac:
                 rmessage = packet[Raw]
+
         if self.key != '':
             cipher = fernet(self.key)
             rmessage = cipher.decrypt(rmessage.load).decode()
+
         else:
             rmessage = rmessage.load.decode('utf-8', errors = 'ignore')
+
         return rmessage
 
     def dialup(self, dest, iface, key):
@@ -58,44 +65,57 @@ running = True
 
 while running:
     message = input('(you):')
+
     if message == ':q':
         pychat.send(' ')
         running = False
         break
+
     elif message == ':r':
         pychat.send(' ')
         print(f'({pychat.dest}):{pychat.receive()}')
+
     elif message[0:2] == ':s':
         info = message.split(' ')
         del info[0]
         info = ' '.join(info)
         pychat.send(info)
+
     elif message == ':rr':
+
         while running:
             print(f'({pychat.dest}):{pychat.receive()}')
             time.sleep(1)
+
     elif message == ':rs':
+
         while running:
             pychat.send(input('(you):'))
             time.sleep(1)
+
     elif message == ':fr':
         pychat.send(' ')
         rpath = os.path.dirname(__file__)
         rname = pychat.receive()
         time.sleep(2)
+
         with open(f'{rpath}/{rname}', 'x') as rfile:
             rfile.write(pychat.receive())
         print(f'({pychat.dest}):{rname}')
+
     elif message[0:3] == ':fs':
         tpath = message.split(' ')
         tpath = tpath[1]
         tname = tpath.split('/')
         tname = tname[-1]
+
         with open(f'{tpath}', 'r') as tfile:
             data = tfile.read()
+
         pychat.send(tname)
         time.sleep(2)
         pychat.send(data)
+
     else:
         pychat.send(message)
         print(f'({pychat.dest}):{pychat.receive()}')
